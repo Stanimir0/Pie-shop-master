ac3526c631dc346cecbfcda58cd77fb2600f5377
Stanimir0 <futboolgod2018@gmail.com>
2023-03-29 00:04:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$("#image").click(function(){
+		$(".nav-bar").slideToggle("swing");
+	
+	});
+	
+	$(window).resize(function(){
+	var w = $(window).width();
+	var navBar = $(".nav-bar");
+	if (w > 320 & navBar.is(":hidden")){
+		navBar.removeAttr("style");
+	}
+	
+	});
+
+});
